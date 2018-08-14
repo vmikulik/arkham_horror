5d5a1e712516c2087e0221c02a159770c874a36d
Vladimir Mikulik <yordun@gmail.com>
2018-08-14 19:09:38 +0200

    version 0.5
    Works in the interpreter

--- chaos.py
+++ chaos.py
@@ -1,19 +1,25 @@
 # this file provides functions for drawing chaos tokens.
+# coding=utf-8
 
-from campaignsettings import campaign_defaults, difficulty_levels
+from json import load
+from random import choice
+campaign_settings = load(open('campaignsettings.json'))
 
 class Token():
     def __init__(self, value):
         self.value = value
 
     def __repr__(self):
-        return "Token({})".format(value)
+        return "Token({})".format(self.value)
     
     def __str__(self):
-        if type(value) is int:
-            return str(value)
-        if type(value) is str:
-            return T.to_symbol(value)
+        if type(self.value) is int:
+            return str(self.value)
+        else:
+            return Token.to_symbol(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value if isinstance(other, Token) else False
     
     @staticmethod
     def to_symbol(string):
@@ -31,15 +37,48 @@ class Token():
         else:
             return string
 
-class ChaosBag():
-    def __init__(self, campaign=None, difficulty=None):
-        campaign, difficulty = campaign.lower(), difficulty.lower()
+class Bag():
+    def __init__(self, campaign=None, difficulty=None, tokens=None):
         if difficulty == None or campaign == None:
-            self.tokens = []
-        elif campaign not in campaign_defaults or difficulty not in difficulty_levels:
-                raise Error("bad input")
+            if tokens == None:
+                tokens = []
+            self.tokens = tokens
         else:
-            self.tokens = [Token(value) for value in ChaosBag.campaign_defaults[campaign][difficulty]]
+            campaign, difficulty = campaign.lower(), difficulty.lower()
+            if campaign not in campaign_settings: 
+                raise Error("invalid campaign")
+            elif difficulty not in ["easy", "standard", "hard", "expert"]:
+                raise Error("invalid difficulty")
+            else:
+                self.tokens = [Token(value) for value in campaign_settings[campaign][difficulty]]
+    
+    def __repr__(self):
+        return "Bag({})".format(repr(self.tokens))
+
+    def __str__(self):
+        return "Bag({})".format(",".join([str(token) for token in self.tokens]))
+
+    def draw(self):
+        print(str(choice(self.tokens)))
+
+    def add(self, value):
+        self.tokens.append(Token(value))
+
+    def remove(self, value):
+        self.tokens.remove(Token(value))
+
+if __name__ == '__main__':
+    bag = Bag()
+    print("Empty bag: " + str(bag))
+    bag = Bag([])
+    print("Empty bag: " + str(bag))
+    bag = Bag("night of the zealot", "easy")
+    print("Night of the Zealot easy bag: " + str(bag))
+    bag = Bag("the dunwich legacy", "standard")
+    print("The Dunwich Legacy standard bag: " + str(bag))
+    bag.add('old one')
+    bag.remove('tentacles')
+    print("with old one but not tentacles: " + str(bag))
 
 
     
